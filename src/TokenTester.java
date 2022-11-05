f2b91711de5393c9edf3e6f5f2289d15e09e047f
MarcinLes7 <lmartino@wp.pl>
2022-11-05 11:58:28 +0100

    Initial commit

---
+++ src/TokenTester.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class TokenTester {
+    public static void main(String[] args) {
+
+        Object obj = new Object();
+        String name = new String("Michael Jordan");
+        Point pt = new Point(0, 0);
+    }
+}
